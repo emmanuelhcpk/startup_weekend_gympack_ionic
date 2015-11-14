30cfb4ead1b887d79048e38516868b4d48d87ed7
emmanuel <eava745@gmail.com>
2015-11-14 09:49:12 -0500

    con pagians de login y signup

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('inicioDeSesióNCtrl', function($scope) {
+
+})
+   
+.controller('registroCtrl', function($scope) {
+
+})
+   
+.controller('rutinasCtrl', function($scope) {
+
+})
+   
+.controller('progresoCtrl', function($scope) {
+
+})
+   
+.controller('establecimientosCtrl', function($scope) {
+
+})
+    
---
+++ www/js/routes.js
@@ -0,0 +1,85 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('inicioDeSesióN', {
+      url: '/page1',
+      templateUrl: 'templates/inicioDeSesióN.html',
+      controller: 'inicioDeSesióNCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('registro', {
+      url: '/page2',
+      templateUrl: 'templates/registro.html',
+      controller: 'registroCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('gymPack.rutinas', {
+      url: '/page4',
+      views: {
+        'tab1': {
+          templateUrl: 'templates/rutinas.html',
+          controller: 'rutinasCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('gymPack.progreso', {
+      url: '/page5',
+      views: {
+        'tab2': {
+          templateUrl: 'templates/progreso.html',
+          controller: 'progresoCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('gymPack.establecimientos', {
+      url: '/page6',
+      views: {
+        'tab3': {
+          templateUrl: 'templates/establecimientos.html',
+          controller: 'establecimientosCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('gymPack', {
+      url: '/page3',
+      abstract:true,
+      templateUrl: 'templates/gymPack.html'
+    })
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page1');
+
+});
